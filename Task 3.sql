b651b02e5055e30509d758beb2a4cbd977b64cf3
gayathri160519 <gayathri160519@gmail.com>
2026-08-22 15:04:20 +0530

    Add files via upload

---
+++ Task 3.sql
@@ -0,0 +1,7 @@
+-- SQL Question 3: ALTER – Modify Column --
+
+use city_hospital;
+
+ALTER TABLE patients MODIFY PatientName VARCHAR(100);
+
+-- PatientName Column in VARCHAR(50) So i modified as VARCHAR(100) --
